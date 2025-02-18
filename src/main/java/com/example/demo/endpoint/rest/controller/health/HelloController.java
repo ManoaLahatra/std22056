9ad4ee9ee92f5ab230b50e1996a98afa5bc9b08f
ManoaLahatra <manoalahatra07@gmail.com>
2025-02-18 08:30:03 +0300

    Update HelloController.java

--- src/main/java/com/example/demo/endpoint/rest/controller/health/HelloController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/health/HelloController.java
@@ -1,8 +1,5 @@
 package com.example.demo.endpoint.rest.controller.health;
 
-import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
